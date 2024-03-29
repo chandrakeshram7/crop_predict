ddbfaddb39118ce7f3b88a339b99d952a89dd9dd
Chandrakesh Ram <chandrakeshram7@gmail.com>
2024-03-29 12:52:01 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # Importing essential libraries and modules
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import io
 import joblib
+from flask_cors import CORS
 crop_recommendation_model_path = 'crop_app'
 crop_recommendation_model = joblib.load(crop_recommendation_model_path)
 
 app = Flask(__name__)
+CORS(app, methods=['GET', 'POST', 'OPTIONS'])
 
 
 @ app.route('/')
@@ -16,19 +18,20 @@ def home():
 
 
 
-@ app.route('/predict')
+@ app.route('/predict', methods=['POST'])
 def crop_prediction():
+        crop_file = request.get_json()
+        print("Received farmer profile:", crop_file)
     
-        N = 23
-        P = 23
-        K = 34
-        ph = 4
-        rainfall = 123.32
-        temperature = 34.33
-        humidity = 22.1
+        # N = 23
+        # P = 23
+        # K = 34
+        # ph = 4
+        # rainfall = 123.32
+        # temperature = 34.33
+        # humidity = 22.1
        
-        data = [[N, P, K, temperature, humidity, ph, rainfall]]
-        my_prediction = crop_recommendation_model.predict(data)
+        my_prediction = crop_recommendation_model.predict(crop_file)
         final_prediction = my_prediction[0]
 
         return jsonify({'prediction ' : final_prediction })
