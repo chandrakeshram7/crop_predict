2b234f2a14797259b13245e5b3b4d41ea91a8e17
Chandrakesh Ram <chandrakeshram7@gmail.com>
2024-04-05 13:37:16 +0530

    Added Minimum Support Price API

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import io
 import joblib
 from flask_cors import CORS
 import numpy as np
+import pandas as pd
 crop_recommendation_model_path = 'crop_app'
 crop_recommendation_model = joblib.load(crop_recommendation_model_path)
 
+df = pd.read_csv('msp.csv')
 app = Flask(__name__)
 CORS(app, methods=['GET', 'POST', 'OPTIONS'])
 
@@ -39,8 +41,33 @@ def crop_prediction():
 
         return jsonify({'prediction' : final_prediction })
 
-        
-#This is just 
+@app.route('/msp')
+def get_msp():
+    data = request.get_json()
+    crop_name = data.get('crop_name')
+    # crop_name ="Jowar"
+    if crop_name is None:
+        return jsonify({'error': 'Crop name parameter is missing'}), 400
+    
+    crop_data = df[df['crop_name'] == crop_name.title()]  # Filter data for the specified crop name
+    
+    if crop_data.empty:
+        return jsonify({'error': 'Crop not found'}), 404
+    
+    msp_dict = {
+        'Crop': crop_name.title(),
+        'Varieties': crop_data['variety'].tolist(),
+        'MSP': {
+            '2021-22': crop_data['2021-22'].tolist(),
+            '2022-23': crop_data['2022-23'].tolist(),
+            '2023-24': crop_data['2023-24'].tolist()
+        }
+    }
+    
+    return jsonify(msp_dict)
+
+    
+
 
 
 # ===============================================================================================
